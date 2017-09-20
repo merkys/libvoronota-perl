f30263e0e587bde6bfa0a73364697eb53c2caef8
Andrius Merkys <andrius.merkys@gmail.com>
2017-09-20 11:32:57 +0000

    voronota-swig/ Removing the need for mock_solvent_ids vector.
    git-svn-id: svn+ssh://saulius-grazulis.lt/home/andrius/svn-repositories/libvoronota-perl/trunk@8 6d0e8d3e-3239-46c8-99d2-6f0b37d48529

--- voronota-swig/Voronota.cpp
+++ voronota-swig/Voronota.cpp
@@ -20,7 +20,6 @@ std::vector<Contact> calculate_contacts( const std::vector<double> &s ) {
 	const int sih_depth = 3;
 
     std::vector<apollota::SimpleSphere> spheres;
-    std::set<std::size_t> mock_solvent_ids;
     for( unsigned long i = 0; i < s.size() / 4; i++ ) {
         spheres.push_back( apollota::SimpleSphere( s[i*4], s[i*4+1], s[i*4+2], s[i*4+3] ) );
     }
@@ -35,28 +34,23 @@ std::vector<Contact> calculate_contacts( const std::vector<double> &s ) {
 	std::map<apollota::Pair, double> interactions_map;
 	std::pair< bool, std::map<std::size_t, double> > volumes_map_bundle(!volumes_output.empty(), std::map<std::size_t, double>());
 
-	{
-		const std::map<apollota::Pair, double> constrained_contacts=apollota::ConstrainedContactsConstruction::construct_contacts(spheres, vertices_vector, probe, step, projections, mock_solvent_ids, volumes_map_bundle);
-		for(std::map<apollota::Pair, double>::const_iterator it=constrained_contacts.begin();it!=constrained_contacts.end();++it)
-		{
-			if(it->first.get(0)<input_spheres_count && it->first.get(1)<input_spheres_count)
-			{
-				interactions_map[it->first]=it->second;
-			}
-		}
-	}
+    const std::map<apollota::Pair, double> constrained_contacts=apollota::ConstrainedContactsConstruction::construct_contacts(spheres, vertices_vector, probe, step, projections, std::set<std::size_t>(), volumes_map_bundle);
+    for(std::map<apollota::Pair, double>::const_iterator it=constrained_contacts.begin();it!=constrained_contacts.end();++it)
+    {
+        if(it->first.get(0)<input_spheres_count && it->first.get(1)<input_spheres_count)
+        {
+            interactions_map[it->first]=it->second;
+        }
+    }
 
-	if(mock_solvent_ids.empty())
-	{
-		const std::map<std::size_t, double> constrained_contact_remainders=apollota::ConstrainedContactsConstruction::construct_contact_remainders(spheres, vertices_vector, probe, sih_depth, volumes_map_bundle);
-		for(std::map<std::size_t, double>::const_iterator it=constrained_contact_remainders.begin();it!=constrained_contact_remainders.end();++it)
-		{
-			if(it->first<input_spheres_count)
-			{
-				interactions_map[apollota::Pair(it->first, it->first)]=it->second;
-			}
-		}
-	}
+    const std::map<std::size_t, double> constrained_contact_remainders=apollota::ConstrainedContactsConstruction::construct_contact_remainders(spheres, vertices_vector, probe, sih_depth, volumes_map_bundle);
+    for(std::map<std::size_t, double>::const_iterator it=constrained_contact_remainders.begin();it!=constrained_contact_remainders.end();++it)
+    {
+        if(it->first<input_spheres_count)
+        {
+            interactions_map[apollota::Pair(it->first, it->first)]=it->second;
+        }
+    }
 
     std::vector<Contact> contacts;
     for(std::map<apollota::Pair, double>::const_iterator it=interactions_map.begin();it!=interactions_map.end();++it)
