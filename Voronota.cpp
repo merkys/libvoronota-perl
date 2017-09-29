49480fc5b1567b3d200f6d66f378726b12f74571
Andrius Merkys <andrius.merkys@gmail.com>
2017-09-29 06:56:21 +0000

    trunk/ Converting tags to spaces.
    git-svn-id: svn+ssh://saulius-grazulis.lt/home/andrius/svn-repositories/libvoronota-perl/trunk@20 6d0e8d3e-3239-46c8-99d2-6f0b37d48529

--- Voronota.cpp
+++ Voronota.cpp
@@ -20,22 +20,22 @@ std::vector<Contact> calculate_contacts( const std::vector<double> &s ) {
     const std::string volumes_output = "";
     const double step = 0.2;
     const int projections = 5;
-	const int sih_depth = 3;
+    const int sih_depth = 3;
 
     std::vector<apollota::SimpleSphere> spheres;
     for( unsigned long i = 0; i < s.size() / 4; i++ ) {
         spheres.push_back( apollota::SimpleSphere( s[i*4], s[i*4+1], s[i*4+2], s[i*4+3] ) );
     }
 
-	const std::size_t input_spheres_count=spheres.size();
-	const std::vector<apollota::SimpleSphere> artificial_boundary=apollota::construct_artificial_boundary(spheres, probe*2.0);
-	spheres.insert(spheres.end(), artificial_boundary.begin(), artificial_boundary.end());
+    const std::size_t input_spheres_count=spheres.size();
+    const std::vector<apollota::SimpleSphere> artificial_boundary=apollota::construct_artificial_boundary(spheres, probe*2.0);
+    spheres.insert(spheres.end(), artificial_boundary.begin(), artificial_boundary.end());
 
-	const apollota::Triangulation::Result triangulation_result=apollota::Triangulation::construct_result(spheres, 3.5, exclude_hidden_balls, false);
-	const apollota::Triangulation::VerticesVector vertices_vector=apollota::Triangulation::collect_vertices_vector_from_quadruples_map(triangulation_result.quadruples_map);
+    const apollota::Triangulation::Result triangulation_result=apollota::Triangulation::construct_result(spheres, 3.5, exclude_hidden_balls, false);
+    const apollota::Triangulation::VerticesVector vertices_vector=apollota::Triangulation::collect_vertices_vector_from_quadruples_map(triangulation_result.quadruples_map);
 
-	std::map<apollota::Pair, double> interactions_map;
-	std::pair< bool, std::map<std::size_t, double> > volumes_map_bundle(!volumes_output.empty(), std::map<std::size_t, double>());
+    std::map<apollota::Pair, double> interactions_map;
+    std::pair< bool, std::map<std::size_t, double> > volumes_map_bundle(!volumes_output.empty(), std::map<std::size_t, double>());
 
     const std::map<apollota::Pair, double> constrained_contacts=apollota::ConstrainedContactsConstruction::construct_contacts(spheres, vertices_vector, probe, step, projections, std::set<std::size_t>(), volumes_map_bundle);
     for(std::map<apollota::Pair, double>::const_iterator it=constrained_contacts.begin();it!=constrained_contacts.end();++it)
@@ -69,9 +69,9 @@ std::vector<Contact> calculate_contacts( const std::vector<double> &s ) {
 
 std::vector<Vertice> calculate_vertices( const std::vector<double> &s ) {
 
-	const bool exclude_hidden_balls = false;
-	const bool include_surplus_quadruples = false;
-	const double init_radius_for_BSH = 3.5;
+    const bool exclude_hidden_balls = false;
+    const bool include_surplus_quadruples = false;
+    const double init_radius_for_BSH = 3.5;
 
     std::vector<apollota::SimpleSphere> spheres;
     for( unsigned long i = 0; i < s.size() / 4; i++ ) {
